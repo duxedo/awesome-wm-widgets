7c50e77275e29035479ace222626321fdc1b66bf
Mikhail Pletnev <mmp.dux@gmail.com>
2023-02-26 15:48:13 +0300

    add notifications, allow vertical autosize for bar widget

--- volume-widget/widgets/horizontal-bar-widget.lua
+++ volume-widget/widgets/horizontal-bar-widget.lua
@@ -12,7 +12,7 @@ function widget.get_widget(widgets_args)
     local main_color = args.main_color or beautiful.fg_normal
     local mute_color = args.mute_color or beautiful.fg_urgent
     local bg_color = args.bg_color or '#ffffff11'
-    local width = args.width or 50
+    local width = args.width
     local margins = args.margins or 10
     local shape = args.shape or 'bar'
     local with_icon = args.with_icon == true and true or false
